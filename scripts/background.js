8e12dfde14011217c11843d6e56c2bc6ef1986e7
Mike Lee <mikelee2082@gmail.com>
2024-01-05 10:22:03 +1300

    updated with first attempt at reading forms

---
+++ scripts/background.js
@@ -0,0 +1,24 @@
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) =>{
+    if (message.action == "submitFormData") {
+        const { url, formData } = message.data;
+        fetch("http://localhost:3100", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ url, formData })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok.');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Data sent: ', data);
+        })
+        .catch(error => {
+            console.error('There was an error sending the data: ', error);
+        });
+    }
+});
---
+++ scripts/content.js
@@ -0,0 +1,11 @@
+document.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    chrome.runtime.sendMessage({
+        action: "submitFormData",
+        data: {
+            url: window.location.href,
+            formData: Object.fromEntries(formData.entries())
+        }
+    });
+});
