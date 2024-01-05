87be59b23e1712830c8088c152be09139fdcbcbc
Mike Lee <mikelee2082@gmail.com>
2024-01-05 16:46:30 +1300

    test more simple content script and worker

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,24 +1,4 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) =>{
-    if (message.action == "submitFormData" || message.action == "input") {
-        const { url, formData } = message.data;
-        fetch("http://localhost:3100", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ url, formData })
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok.');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Data sent: ', data);
-        })
-        .catch(error => {
-            console.error('There was an error sending the data: ', error);
-        });
-    }
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log(message);
+    sendResponse({ message: "Response from service worker" });
 });
--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,23 +1,8 @@
-document.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    chrome.runtime.sendMessage({
-        action: "submitFormData",
-        data: {
-            url: window.location.href,
-            formData: Object.fromEntries(formData.entries())
-        }
-    });
-});
-document.addEventListener("input", (event) => {
-    console.log(event.target.value);
-    chrome.runtime.sendMessage({
-        action: "input",
-        data: {
-            url: window.location.href,
-            formData: {
-                fuck: event.target.value
-            }
-        }
-    });
-});
+chrome.runtime.sendMessage(
+    {
+        message: "Hi message from content script"
+    },
+    (response) => {
+        console.log(response.message);
+    }
+);
